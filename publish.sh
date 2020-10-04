cbc56d244efaff83155b451503ff5888ea279522
YuYang <744727849@qq.com>
2020-10-04 21:01:54 +0800

    [update] update blog

--- publish.sh
+++ publish.sh
@@ -11,8 +11,14 @@ echo "\033[33m    |_| \__,_||_|\__,_|_| |_|\__, | \033[0m"
 echo "\033[33m                              __/ |\033[0m"
 echo "\033[33m                             |___/ \033[0m"
 
-# 
+# 添加修改 
 
 git add  .
 
-git commit --amend
+# 提交修改
+
+git commit -m "[update] update blog"
+
+# git push
+
+git push -f
